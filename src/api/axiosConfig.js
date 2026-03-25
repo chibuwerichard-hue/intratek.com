f35e9d5f5b7edf76ec45f9d64fc93b945eac8071
Richard Chibuwe <chibuwerichard@gmail.com>
2026-03-25 14:59:13 +0200

    Connect frontend to Render backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
- 
+
 const api = axios.create({
   baseURL: 'https://supermarket-backend-2-zel8.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
- 
+
 export default api;
- 
